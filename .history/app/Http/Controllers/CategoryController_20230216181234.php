bd6a6466b927f09e15f53b5027b8264e378e9ee9
Harvir Brar <83703941+harvir619@users.noreply.github.com>
2023-02-27 10:50:18 +0800

    Create CategoryController_20230216181234.php

---
+++ .history/app/Http/Controllers/CategoryController_20230216181234.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use ProtoneMedia\Splade\SpladeTable;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::class;
+
+        // return view('categories.index',compact('categories'));
+
+        return view('categories.index', [
+            'categories' => SpladeTable::for($categories)
+                ->column('name', sortable: true,)
+                ->column('slug', label: 'Description', searchable: true)
+                ->column('action')
+                ->withGlobalSearch(columns: ['name', 'slug'])
+                ->paginate(6),
+        ]);
+    }
+}
