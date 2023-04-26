7e64916e9811da6d627ef6973b955fca878bc6fa
Harvir Brar <83703941+harvir619@users.noreply.github.com>
2023-04-26 17:51:14 +0800

    Create PostController_20230216160200.php

---
+++ .history/app/Http/Controllers/PostController_20230216160200.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use ProtoneMedia\Splade\SpladeTable;
+use Spatie\QueryBuilder\AllowedFilter;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::class;
+
+        // return view('categories.index',compact('categories'));
+
+        $globalSearch = AllowedFilter::callback('global',function($query,$value){
+            $query->where
+        })
+        
+        
+        return view('posts.index', [
+            'posts' => SpladeTable::for($posts)
+                ->column('title', sortable: true,)
+                ->column('slug', label: 'Description', searchable: true)
+                ->withGlobalSearch(columns: ['title', 'slug'])
+                ->paginate(6),
+        ]);
+    }
+}
