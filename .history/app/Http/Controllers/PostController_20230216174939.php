b652f4fce27d022aa9ec1d9f2137da526d73eb65
Harvir Brar <83703941+harvir619@users.noreply.github.com>
2023-05-12 15:24:30 +0800

    Create PostController_20230216174939.php

---
+++ .history/app/Http/Controllers/PostController_20230216174939.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+use ProtoneMedia\Splade\SpladeTable;
+use Spatie\QueryBuilder\QueryBuilder;
+use Spatie\QueryBuilder\AllowedFilter;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        // $posts = Post::class;
+
+        // return view('categories.index',compact('categories'));
+
+        $globalSearch = AllowedFilter::callback('global', function ($query, $value) {
+            $query->where(function ($query) use ($value) {
+                Collection::wrap($value)->each(function ($value) use ($query) {
+                    $query
+                        ->orWhere('title', 'LIKE', "%{$value}%")
+                        ->orWhere('slug', 'LIKE', "%{$value}%");
+                });
+            });
+        });
+
+        $posts = QueryBuilder::for(Post::class)
+            ->defaultSort('title')
+            ->allowedSorts(['title', 'slug'])
+            ->allowedFilters(['title', 'slug', 'category_id', $globalSearch]);
+
+
+        return view('posts.index', [
+            'posts' => SpladeTable::for($posts)
+                ->column('title', sortable: true,)
+                ->column('slug', label: 'Description', searchable: true)
+                ->withGlobalSearch(columns: ['title', 'slug'])
+                ->paginate(8),
+        ]);
+    }
+}
